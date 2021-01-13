2eacbf7a6cdc4bf44cdf6cdbdb9e73ae9e4cd0ac
iingot <73196284+iingot@users.noreply.github.com>
2021-01-12 23:01:26 -0600

    Add files via upload

---
+++ Ruby/wpscan-master/CHANGELOG.md
@@ -0,0 +1,372 @@
+# Changelog
+## Master
+[Work in progress](https://github.com/wpscanteam/wpscan/compare/2.6...master)
+
+## Version 2.6
+Released: 2014-12-19
+
+New
+* Updates the readmes to reflect the new --usernames option
+* Improves plugin/theme version detection by looking at the "Version:"
+* Solution to avoid mandatory blank newline at the end of the wordlist
+* Add check for valid credentials
+* Add Sucuri sponsor to banner
+* Add protocol to sucuri url in banner
+* Add response code to proxy error output
+* Add a statement about mendatory newlines at the end of list
+* Give warning if default username 'admin' is still used
+* License amendment to make it more clear about value added usage
+
+Removed
+* remove malwares
+* remove malware folder
+* Removes the theme version check from the readme, unrealistic scenario
+
+General core
+* Update to Ruby 2.1.5 and travis
+* Prevent parent theme infinite loop
+* Fixes the progressbar being overriden by next brute forcing attempts
+
+Fixed issues
+* Fix UTF-8 encode on security db file download
+* Fix #703 - Disable logging by default. Implement log option.
+* Fix #705 - Installation instructions for Ubuntu < 14.04 apparently incomplete
+* Fix #717 - Expand on readme.html finding output
+* Fix #716 - Adds the --version in the help
+* Fix #715 - Add new updating info to docs
+* Fix #727 - WpItems detection: Perform the passive check and filter only vulnerable results at the end if required
+* Fix #737 - Adds some readme files to check for plugin versions
+* Fix #739 - Adds the --usernames option
+
+WPScan Database Statistics:
+* Total vulnerable versions: 88
+* Total vulnerable plugins: 901
+* Total vulnerable themes: 313
+* Total version vulnerabilities: 1050
+* Total plugin vulnerabilities: 1355
+* Total theme vulnerabilities: 349
+
+## Version 2.5.1
+Released: 2014-09-29
+
+Fixes reference URL to WPVDB
+
+## Version 2.5
+Released: 2014-09-26 (@ BruCON 2014)
+
+New
+* Exit program after --update
+* Detect directory listing in upload folder
+* Be more verbose when no version can be detected
+* Added detection for Yoast Wordpress SEO plugin
+* Also ensure to not process empty Location headers
+* Ensures a nil location is not processed when enumerating usernames
+* Fix #626 - Detect 'Must_Use_Plugins'
+* better username extraction
+* Add a --cookie option. Ref #485
+* Add a --no-color option
+* Output: Give 'Fixed in' an informational tag
+* Added ArchAssault distro - WPScan comes pre-installed with this distro
+* Layout changes with new colors
+
+Removed
+* Removes the source code updaters
+* Removes the ListGenerator plugin from WPStools
+* Removes all files from data/
+
+General core
+* Update docs to reflect new updating logic
+* Little output change and coloring
+* Adds a missing verbose output
+* Re-build redirection url if begin with slash '/'
+* Fixes the remove_conditional_comments function
+* Ensures to give a string to Typhoeus
+* Fix wpstools check-vuln-ref-urls
+* Fix rspecs for new json
+* Only output if different from style_url
+* Add exception so 'ruby wpscan.rb http://domain.com' is detected
+* Added make to Debian installation, which is needed in minimal installation.
+* Add build-essentials requirement to Ubuntu > 14.04
+* Updated installation instr. for GNU/Linux Debian.
+* Changes VersionCompare#is_newer_or_same? by lesser_or_equal?
+* Fixes the location of the robots.txt check
+* Updates the recommended ruby version
+* Rspec 3.0 support
+* Adds ruby 2.1.2 to Travis
+* Updated ruby-progressbar to 1.5.0
+
+WordPress Fingerprints
+* Adds WP 4.0 fingerprints
+* Adds WP 3.9.2, 3.8.4 & 3.7.4 fingerprints - Ref #652
+* Adds 3.9.1 fingerprints
+
+Fixed issues
+* Fix #689 - Adds config file to check
+* Fix #694 - Output Arrays
+* Fix #693 - Adds pathname require statement
+* Fix #657 - generate method
+* Fix #685 - Potenial fix for 'marshal data too short' error
+* Fix #686 - Adds specs for relative URI in Location headers
+* Fix #435 - Update license
+* Fix #674 - Improves the Plugins & Themes passive detection
+* Fix #673 - Problem with the output
+* Fix #661 - Don't hash directories named like a file
+* Fix #653 - Fix for infinite loop in wpstools
+* Fix #625 - Only parse styles when needed
+* Fix #481 - Fix for Jetpack plugin false positive
+* Fix #480 - Properly removes the colour sequence from log
+* Fix #472 - WPScan stops after redirection if not WordPress website
+* Fix #464 - Readmes updated to reflect recent changes about the config file & batch mode
+
+Vulnerabilities
+* geoplaces4 also uses name GeoPlaces4beta
+* Added metasploit module's
+* Added some timthumb detections
+
+WPScan Database Statistics:
+* Total vulnerable versions: 87
+* Total vulnerable plugins: 854
+* Total vulnerable themes: 303
+* Total version vulnerabilities: 752
+* Total plugin vulnerabilities: 1351
+* Total theme vulnerabilities: 345
+
+## Version 2.4
+Released: 2014-04-17
+
+New
+* '--batch' switch option added - Fix #454
+* Add random-agent
+* Added more CLI options
+* Switch over to nist - Fix #301
+* New choice added when a redirection is detected - Fix #438
+
+Removed
+* Removed 'Total WordPress Sites in the World' counter from stats
+* Old wpscan repo links removed - Fix #440
+* Fingerprinting Dev script removed
+* Useless code removed
+
+General core
+* Rspecs update
+* Forcing Travis notify the team
+* Ruby 2.1.1 added to Travis
+* Equal output layout for interaction questions
+* Only output error trace if verbose if enabled
+* Memory improvements during wp-items enumerations
+* Fixed broken link checker, fixed some broken links
+* Couple more 404s fixed
+* Themes & Plugins list updated
+
+WordPress Fingerprints
+* WP 3.8.2 & 3.7.2 Fingerprints added - Fix #448
+* WP 3.8.3 & 3.7.3 fingerprints
+* WP 3.9 fingerprints
+
+Fixed issues
+* Fix #380 - Redirects in WP 3.6-3.0
+* Fix #413 - Check the version of the Timthumbs files found
+* Fix #429 - Error WpScan Cache Browser
+* Fix #431 - Version number comparison between '2.3.3' and '0.42b'
+* Fix #439 - Detect if the target goes down during the scan
+* Fix #451 - Do not rely only on files in wp-content for fingerprinting
+* Fix #453 - Documentation or inplemention of option parameters
+* Fix #455 - Fails with a message if the target returns a 403 during the wordpress check
+
+Vulnerabilities
+* Update WordPress Vulnerabilities
+* Fixed some duplicate vulnerabilities
+
+WPScan Database Statistics:
+* Total vulnerable versions: 79; 1 is new
+* Total vulnerable plugins: 748; 55 are new
+* Total vulnerable themes: 292; 41 are new
+* Total version vulnerabilities: 617; 326 are new
+* Total plugin vulnerabilities: 1162; 146 are new
+* Total theme vulnerabilities: 330; 47 are new
+
+## Version 2.3
+Released: 2014-02-11
+
+New
+* Brute forcing over https!
+* Detect and output parent theme!
+* Complete fingerprint script & hash search
+* New spell checker!
+* Added database modification dates in status report
+* Added 'Total WordPress Sites in the World' statistics
+* Added separator between Name and Version in Item
+* Added a "Work in progress" URL in the CHANGELOG
+
+Removed
+* Removed "Exiting!" sentence
+* Removed Backtrack Linux. Not maintained anymore.
+
+General core
+* Ruby 2.1.0 added to Travis
+* Updated the version of WebMock required
+* Better string concatenation in code (improves speed)
+* Some modifications in the output of an item
+* Output cosmetics
+* rspec-mocks version constraint released
+* Tabs replaced by spaces
+* Rspecs update
+* Indent code cleanup
+* Themes & Plugins lists regenerated
+
+Vulnerabilities
+* Update WordPress Vulnerabilities
+* Disabled some fake reported vulnerabilities
+* Fixed some duplicate vulnerabilities
+
+WPScan Database Statistics:
+* Total vulnerable versions: 78; 2 are new
+* Total vulnerable plugins: 693; 83 are new
+* Total vulnerable themes: 251; 55 are new
+* Total version vulnerabilities: 291 17 are new
+* Total plugin vulnerabilities: 1016; 236 are new
+* Total theme vulnerabilities: 283; 79 are new
+
+WordPress Fingerprints
+* Better fingerprints
+* WP 3.8.1 Fingerprinting
+* WP 3.8 Fingerprinting
+
+Fixed issues
+* Fix #404 - Brute forcing issue over https
+* Fix #398 - Removed a fake vuln in WP Super Cache
+* Fix #393 - sudo added to the bundle install cmd for Mac OSX
+* Fix #228, #327 - Infinite loop when self-redirect
+* Fix #201 - Incorrect Paramter Parsing when no url was supplied
+
+## Version 2.2
+Released: 2013-11-12
+
+New
+* Output the vulnerability fix if available
+* Added 'WordPress Version Vulnerability' statistics
+* Added Kali Linux on the list of pre-installed Linux distributions
+* Added hosted wordpress detection. See issue #343.
+* Add detection for all-in-one-seo-pack
+* Use less memory when brute forcing with a large wordlist
+* Memory Usage output
+* Added cve tag to xml file
+* Add documentation to readme
+* Add --version switch
+* Parse robots.txt
+* Show twitter usernames
+* Clean logfile on wpstools too
+* Added pingback header
+* Request_timeout and connect_timeout implemented
+* Output interesting http-headers
+* Kali Linux detection
+* Ensure that brute forcing results are output even if an error occurs or the user exits
+* Added debug output
+* Fixed Version compare for issue #179
+* Added ruby-progressbar version to Gemfile
+* Use the redirect_to parameter on bruteforce
+* Readded "junk removal" from usernames before output
+* Add license file
+* Output the timthumb version if found
+* New enumeration system
+* More error details for XSD checks
+* Added default wp-content dir detection, see Issue #141.
+* Added checks for well formed xml
+
+Changed
+* Trying a fix for Kali Linux
+* Make a seperator between plugin name and vulnerability name
+* It's WordPress, not Wordpress
+* Changed wordpress.com scanning error to warning. See issue #343.
+* Make output lines consistent
+* Replace packetstormsecurity.org to packetstormsecurity.com
+* Same URL syntax for all Packet Storm Security URL's
+* Packet Storm Security URL's don't need the 'friendly part' of the URL. So it can be neglected.
+* Use online documentation
+* User prompt on same line
+* Don't skip passwords that start with a hash. This is fairly common (see RockYou list for example).
+* Updated Fedora install instructions as per Issue #92
+* Slight update to security plugin warning. Issue #212.
+* Ruby-progressbar Gemfile version bump
+* Fix error with the -U option (undefined method 'merge' for #WpTarget:)
+* Banner artwork
+* Fix hacks.rb conflict
+* Handle when there are 2 headers of the same name
+* Releasing the Typhoeus version constraint
+* Amended Arch Linux install instructions. See issue #183.
+
+Updated
+* Plugins & Themes updated
+* Update README.md
+* Updated documentation
+
+Removed
+* Removed 'smileys' in output messages
+* Removed 'for WordPress' and 'plugin' in title strings.
+* Removed reference
+* Removed useless code
+* Removed duplicate vulnerabilities
+
+General core
+* Code cleaning
+* Fix typo's
+* Clean up rspecs
+* Themes & Plugins lists regenerated
+* Rspecs update
+* Code Factoring
+* Added checks for old ruby. Otherwise there will be syntax errors
+
+Vulnerabilities
+* Update WordPress Vulnerabilities
+* Update timthumb due to Secunia #54801
+* Added WP vuln: 3.4 - 3.5.1 wp-admin/users.php FPD
+
+WPScan Database Statistics:
+* Total vulnerable versions: 76; 4 are new
+* Total vulnerable plugins: 610; 201 are new
+* Total vulnerable themes: 196; 47 are new
+* Total version vulnerabilities: 274; 53 are new
+* Total plugin vulnerabilities: 780; 286 are new
+* Total theme vulnerabilities: 204; 52 are new
+
+Add WP Fingerprints
+* WP 3.7.1 Fingerprinting
+* WP 3.7 Fingerprinting
+* Ref #280 WP 3.6.1 fingerprint
+* Added WP 3.6 advanced fingerprint hash. See Issue #255.
+* Updated MD5 hash of WP 3.6 detection. See Issue #277.
+* WP 3.5.2 Fingerprint
+* Bug Fix : Wp 3.5 & 3.5.1 not detected from advanced fingerprinting.
+
+Fixed issues
+* Fix #249 - [ERROR] "\xF1" on US-ASCII
+* Fix #275 - [ERROR] "\xC3" on US-ASCII
+* Fix #271 - Further Instructions added to the Mac Install
+* Fix #266 - passive detection regex
+* Fix #265 - remove base64 images before passive detection
+* Fix #262 - [ERROR] bad component(expected absolute path component)
+* Fix #260 - Fixes Travis Fail, due to rspec-mock v2.14.3
+* Fix #208 - Fixed vulnerable plugins still appear in the results
+* Fix #245 - all theme enumeration error
+* Fix #241 - Cant convert array to string
+* Fix #232 - Crash while enumerating usernames
+* Fix #223 - New wordpress urls for most popular plugins & themes
+* Fix #177 - Passive Cache plugins detection (no spec)
+* Fix #169 - False reports
+* Fix #182 - Remove the progress-bar static length (120), and let it to automatic
+* Fix #181 - Don't exit if no usernames found during a simple enumeration (but exit if a brute force is asked)
+* Fix #200 - Log file not recording the list of username retireved
+* Fix #164 - README.txt detection
+* Fix #166 - ListGenerator using the old Browser#get method for full generation
+* Fix #153 - Disable error trace when it's from the main script
+* Fix #163 - in the proper way
+* Fix #144 - Use cookie jar to prevent infinite redirections loop
+* Fix #158 - Add the solution to 'no such file to load -- rubygems' in the README
+* Fix #152 - invalid ssl_certificate - response code 0
+* Fix #147 - can't modify frozen string
+* Fix #140 - xml_rpc_url in the body
+* Fix #153 - No error trace when 'No argument supplied'
+
+## Version 2.1
+Released 2013-3-4
+
---
+++ Ruby/wpscan-master/wpscan.rb
@@ -0,0 +1,416 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+$: << '.'
+require File.dirname(__FILE__) + '/lib/wpscan/wpscan_helper'
+
+def main
+  # delete old logfile, check if it is a symlink first.
+  File.delete(LOG_FILE) if File.exist?(LOG_FILE) and !File.symlink?(LOG_FILE)
+
+  begin
+    wpscan_options = WpscanOptions.load_from_arguments
+
+    $log = wpscan_options.log
+
+    banner() # called after $log set
+
+    unless wpscan_options.has_options?
+      # first parameter only url?
+      if ARGV.length == 1
+        wpscan_options.url = ARGV[0]
+      else
+        usage()
+        raise('No argument supplied')
+      end
+    end
+
+    # Define a global variable
+    $COLORSWITCH = wpscan_options.no_color
+
+    if wpscan_options.help
+      help()
+      usage()
+      exit(0)
+    end
+
+    if wpscan_options.version
+      puts "Current version: #{WPSCAN_VERSION}"
+      exit(0)
+    end
+
+    # Initialize the browser to allow the db update
+    # to be done over a proxy if set
+    Browser.instance(
+      wpscan_options.to_h.merge(max_threads: wpscan_options.threads)
+    )
+
+    if wpscan_options.update || missing_db_file?
+      puts "#{notice('[i]')} Updating the Database ..."
+      DbUpdater.new(DATA_DIR).update(wpscan_options.verbose)
+      puts "#{notice('[i]')} Update completed."
+      # Exit program if only option --update is used
+      exit(0) unless wpscan_options.url
+    end
+
+    unless wpscan_options.url
+      raise 'The URL is mandatory, please supply it with --url or -u'
+    end
+
+    wp_target = WpTarget.new(wpscan_options.url, wpscan_options.to_h)
+
+    # Remote website up?
+    unless wp_target.online?
+      raise "The WordPress URL supplied '#{wp_target.uri}' seems to be down."
+    end
+
+    if wpscan_options.proxy
+      proxy_response = Browser.get(wp_target.url)
+
+      unless WpTarget::valid_response_codes.include?(proxy_response.code)
+        raise "Proxy Error :\r\nResponse Code: #{proxy_response.code}\r\nResponse Headers: #{proxy_response.headers}"
+      end
+    end
+
+    # Remote website has a redirection?
+    if (redirection = wp_target.redirection)
+      if wpscan_options.follow_redirection
+        puts "Following redirection #{redirection}"
+      else
+        puts "#{notice('[i]')} The remote host tried to redirect to: #{redirection}"
+        print '[?] Do you want follow the redirection ? [Y]es [N]o [A]bort, default: [N]'
+      end
+      if wpscan_options.follow_redirection || !wpscan_options.batch
+        if wpscan_options.follow_redirection || (input = Readline.readline) =~ /^y/i
+          wpscan_options.url = redirection
+          wp_target = WpTarget.new(redirection, wpscan_options.to_h)
+        else
+          if input =~ /^a/i
+            puts 'Scan aborted'
+            exit(0)
+          end
+        end
+      end
+    end
+
+    if wp_target.has_basic_auth? && wpscan_options.basic_auth.nil?
+      raise 'Basic authentication is required, please provide it with --basic-auth <login:password>'
+    end
+
+    # test for valid credentials
+    unless wpscan_options.basic_auth.nil?
+      res = Browser.get_and_follow_location(wp_target.url)
+      raise 'Invalid credentials supplied' if res && res.code == 401
+    end
+
+    # Remote website is wordpress?
+    unless wpscan_options.force
+      unless wp_target.wordpress?
+        raise "#{critical('[!]')} The remote website is up, but does not seem to be running WordPress."
+      end
+    end
+
+    unless wp_target.wp_content_dir
+      raise 'The wp_content_dir has not been found, please supply it with --wp-content-dir'
+    end
+
+    unless wp_target.wp_plugins_dir_exists?
+      puts "The plugins directory '#{wp_target.wp_plugins_dir}' does not exist."
+      puts 'You can specify one per command line option (don\'t forget to include the wp-content directory if needed)'
+      puts '[?] Continue? [Y]es [N]o, default: [N]'
+      if wpscan_options.batch || Readline.readline !~ /^y/i
+        exit(0)
+      end
+    end
+
+    # Output runtime data
+    start_time   = Time.now
+    start_memory = get_memory_usage
+    puts "#{info('[+]')} URL: #{wp_target.url}"
+    puts "#{info('[+]')} Started: #{start_time.asctime}"
+    puts
+
+    if wp_target.wordpress_hosted?
+      puts "#{critical('[!]')} We do not support scanning *.wordpress.com hosted blogs"
+    end
+
+    if wp_target.has_robots?
+      puts "#{info('[+]')} robots.txt available under: '#{wp_target.robots_url}'"
+
+      wp_target.parse_robots_txt.each do |dir|
+        puts "#{info('[+]')} Interesting entry from robots.txt: #{dir}"
+      end
+    end
+
+    if wp_target.has_readme?
+      puts "#{warning('[!]')} The WordPress '#{wp_target.readme_url}' file exists exposing a version number"
+    end
+
+    if wp_target.has_full_path_disclosure?
+      puts "#{warning('[!]')} Full Path Disclosure (FPD) in: '#{wp_target.full_path_disclosure_url}'"
+    end
+
+    if wp_target.has_debug_log?
+      puts "#{critical('[!]')} Debug log file found: #{wp_target.debug_log_url}"
+    end
+
+    wp_target.config_backup.each do |file_url|
+      puts "#{critical('[!]')} A wp-config.php backup file has been found in: '#{file_url}'"
+    end
+
+    if wp_target.search_replace_db_2_exists?
+      puts "#{critical('[!]')} searchreplacedb2.php has been found in: '#{wp_target.search_replace_db_2_url}'"
+    end
+
+    wp_target.interesting_headers.each do |header|
+      output = "#{info('[+]')} Interesting header: "
+
+      if header[1].class == Array
+        header[1].each do |value|
+          puts output + "#{header[0]}: #{value}"
+        end
+      else
+        puts output + "#{header[0]}: #{header[1]}"
+      end
+    end
+
+    if wp_target.multisite?
+      puts "#{info('[+]')} This site seems to be a multisite (http://codex.wordpress.org/Glossary#Multisite)"
+    end
+
+    if wp_target.has_must_use_plugins?
+      puts "#{info('[+]')} This site has 'Must Use Plugins' (http://codex.wordpress.org/Must_Use_Plugins)"
+    end
+
+    if wp_target.registration_enabled?
+      puts "#{warning('[+]')} Registration is enabled: #{wp_target.registration_url}"
+    end
+
+    if wp_target.has_xml_rpc?
+      puts "#{info('[+]')} XML-RPC Interface available under: #{wp_target.xml_rpc_url}"
+      puts "#{notice('[i]')} This may allow the GHOST vulnerability to be exploited, please see: https://www.rapid7.com/db/modules/auxiliary/scanner/http/wordpress_ghost_scanner"
+    end
+
+    if wp_target.upload_directory_listing_enabled?
+      puts "#{warning('[!]')} Upload directory has directory listing enabled: #{wp_target.upload_dir_url}"
+    end
+
+    enum_options = {
+      show_progression: true,
+      exclude_content: wpscan_options.exclude_content_based
+    }
+
+    if wp_version = wp_target.version(WP_VERSIONS_FILE)
+      wp_version.output(wpscan_options.verbose)
+    else
+      puts
+      puts "#{notice('[i]')} WordPress version can not be detected"
+    end
+
+    if wp_theme = wp_target.theme
+      puts
+      # Theme version is handled in #to_s
+      puts "#{info('[+]')} WordPress theme in use: #{wp_theme}"
+      wp_theme.output(wpscan_options.verbose)
+
+      # Check for parent Themes
+      parent_theme_count = 0
+      while wp_theme.is_child_theme? && parent_theme_count <= wp_theme.parent_theme_limit
+        parent_theme_count += 1
+
+        parent = wp_theme.get_parent_theme
+        puts
+        puts "#{info('[+]')} Detected parent theme: #{parent}"
+        parent.output(wpscan_options.verbose)
+        wp_theme = parent
+      end
+
+    end
+
+    if wpscan_options.enumerate_plugins == nil and wpscan_options.enumerate_only_vulnerable_plugins == nil
+      puts
+      puts "#{info('[+]')} Enumerating plugins from passive detection ..."
+
+      wp_plugins = WpPlugins.passive_detection(wp_target)
+      if !wp_plugins.empty?
+        puts " | #{wp_plugins.size} plugins found:"
+
+        wp_plugins.output(wpscan_options.verbose)
+      else
+        puts "#{info('[+]')} No plugins found"
+      end
+    end
+
+    # Enumerate the installed plugins
+    if wpscan_options.enumerate_plugins or wpscan_options.enumerate_only_vulnerable_plugins or wpscan_options.enumerate_all_plugins
+      puts
+      puts "#{info('[+]')} Enumerating installed plugins #{'(only vulnerable ones)' if wpscan_options.enumerate_only_vulnerable_plugins} ..."
+      puts
+
+      wp_plugins = WpPlugins.aggressive_detection(wp_target,
+        enum_options.merge(
+          file: wpscan_options.enumerate_all_plugins ? PLUGINS_FULL_FILE : PLUGINS_FILE,
+          only_vulnerable: wpscan_options.enumerate_only_vulnerable_plugins || false
+        )
+      )
+      puts
+      if !wp_plugins.empty?
+        puts "#{info('[+]')} We found #{wp_plugins.size} plugins:"
+
+        wp_plugins.output(wpscan_options.verbose)
+      else
+        puts "#{info('[+]')} No plugins found"
+      end
+    end
+
+    # Enumerate installed themes
+    if wpscan_options.enumerate_themes or wpscan_options.enumerate_only_vulnerable_themes or wpscan_options.enumerate_all_themes
+      puts
+      puts "#{info('[+]')} Enumerating installed themes #{'(only vulnerable ones)' if wpscan_options.enumerate_only_vulnerable_themes} ..."
+      puts
+
+      wp_themes = WpThemes.aggressive_detection(wp_target,
+        enum_options.merge(
+          file: wpscan_options.enumerate_all_themes ? THEMES_FULL_FILE : THEMES_FILE,
+          only_vulnerable: wpscan_options.enumerate_only_vulnerable_themes || false
+        )
+      )
+      puts
+      if !wp_themes.empty?
+        puts "#{info('[+]')} We found #{wp_themes.size} themes:"
+
+        wp_themes.output(wpscan_options.verbose)
+      else
+        puts "#{info('[+]')} No themes found"
+      end
+    end
+
+    if wpscan_options.enumerate_timthumbs
+      puts
+      puts "#{info('[+]')} Enumerating timthumb files ..."
+      puts
+
+      wp_timthumbs = WpTimthumbs.aggressive_detection(wp_target,
+        enum_options.merge(
+          file: DATA_DIR + '/timthumbs.txt',
+          theme_name: wp_theme ? wp_theme.name : nil
+        )
+      )
+      puts
+      if !wp_timthumbs.empty?
+        puts "#{info('[+]')} We found #{wp_timthumbs.size} timthumb file/s:"
+
+        wp_timthumbs.output(wpscan_options.verbose)
+      else
+        puts "#{info('[+]')} No timthumb files found"
+      end
+    end
+
+    # If we haven't been supplied a username/usernames list, enumerate them...
+    if !wpscan_options.username && !wpscan_options.usernames && wpscan_options.wordlist || wpscan_options.enumerate_usernames
+      puts
+      puts "#{info('[+]')} Enumerating usernames ..."
+
+      if wp_target.has_plugin?('stop-user-enumeration')
+        puts "#{warning('[!]')} Stop User Enumeration plugin detected, results might be empty. " \
+             "However a bypass exists for v1.2.8 and below, see stop_user_enumeration_bypass.rb in #{File.expand_path(File.dirname(__FILE__))}"
+      end
+
+      wp_users = WpUsers.aggressive_detection(wp_target,
+        enum_options.merge(
+          range: wpscan_options.enumerate_usernames_range,
+          show_progression: false
+        )
+      )
+
+      if wp_users.empty?
+        puts "#{info('[+]')} We did not enumerate any usernames"
+
+        if wpscan_options.wordlist
+          puts 'Try supplying your own username with the --username option'
+          puts
+          exit(1)
+        end
+      else
+        puts "#{info('[+]')} Identified the following #{wp_users.size} user/s:"
+        wp_users.output(margin_left: ' ' * 4)
+        if wp_users[0].login == "admin"
+           puts "#{warning('[!]')} Default first WordPress username 'admin' is still used"
+        end
+      end
+
+    else
+      wp_users = WpUsers.new
+
+      if wpscan_options.usernames
+        File.open(wpscan_options.usernames).each do |username|
+          wp_users << WpUser.new(wp_target.uri, login: username.chomp)
+        end
+      else
+        wp_users << WpUser.new(wp_target.uri, login: wpscan_options.username)
+      end
+    end
+
+    # Start the brute forcer
+    bruteforce = true
+    if wpscan_options.wordlist
+      if wp_target.has_login_protection?
+
+        protection_plugin = wp_target.login_protection_plugin()
+
+        puts
+        puts "#{warning('[!]')} The plugin #{protection_plugin.name} has been detected. It might record the IP and timestamp of every failed login and/or prevent brute forcing altogether. Not a good idea for brute forcing!"
+        puts '[?] Do you want to start the brute force anyway ? [Y]es [N]o, default: [N]'
+
+        bruteforce = false if wpscan_options.batch || Readline.readline !~ /^y/i
+      end
+
+      if bruteforce
+        puts "#{info('[+]')} Starting the password brute forcer"
+
+        begin
+          wp_users.brute_force(
+            wpscan_options.wordlist,
+            show_progression: true,
+            verbose: wpscan_options.verbose
+          )
+        ensure
+          puts
+          wp_users.output(show_password: true, margin_left: ' ' * 2)
+        end
+      else
+        puts "#{critical('[!]')} Brute forcing aborted"
+      end
+    end
+
+    stop_time   = Time.now
+    elapsed     = stop_time - start_time
+    used_memory = get_memory_usage - start_memory
+
+    puts
+    puts info("[+] Finished: #{stop_time.asctime}")
+    puts info("[+] Requests Done: #{@total_requests_done}")
+    puts info("[+] Memory used: #{used_memory.bytes_to_human}")
+    puts info("[+] Elapsed time: #{Time.at(elapsed).utc.strftime('%H:%M:%S')}")
+    exit(0) # must exit!
+
+  rescue SystemExit, Interrupt
+
+  rescue => e
+    puts
+    puts critical(e.message)
+
+    if wpscan_options && wpscan_options.verbose
+      puts critical('Trace:')
+      puts critical(e.backtrace.join("\n"))
+    end
+    exit(1)
+  ensure
+    # Ensure a clean abort of Hydra
+    # See https://github.com/wpscanteam/wpscan/issues/461#issuecomment-42735615
+    Browser.instance.hydra.abort
+    Browser.instance.hydra.run
+  end
+end
+
+main()
